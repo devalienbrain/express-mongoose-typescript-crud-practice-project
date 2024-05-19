7bb5580a042a3f709957a13c710b8b1b95be891c
Alien Brain <murad.dub@gmail.com>
2024-05-20 03:13:22 +0600

    interface-schema-model created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 // const express = require("express");
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { StudentRoutes } from "./modules/student/student.route";
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+
+// Application Routes
+app.use("/api/v1/students", StudentRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   // const a = 10;
